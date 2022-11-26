50162deddaed03eb34c2a3564a9414961e7c8195
amy <amy@froggery.dev>
2022-11-26 20:41:08 +0100

    update stuff

--- src/Math.sol
+++ src/Math.sol
@@ -1,24 +1,29 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.15;
 
-import "prb/math/contracts/PRBMathUD60x18Typed.sol";
+import "lib/prb-math/contracts/PRBMathUD60x18Typed.sol";
 
 contract Math {
 
+    uint private constant secondsInYear = 365 * 24 * 60 * 60;
+
     struct Loan {
         uint takeoutTime;
-        uint principal;
-        uint totalRepaid;
+        PRBMath.UD60x18 principal;
+        PRBMath.UD60x18 totalRepaid;
     }
 
-    uint private constant secondsInYear = 365 * 24 * 60 * 60;
-    PRBMath.UD60x18 private ratePerSecond = 5.div(100).div(secondsInYear);
+    using PRBMathUD60x18Typed for PRBMath.UD60x18;
+    using PRBMathUD60x18Typed for uint;
 
-    function loanSeconds(Loan memory loan) private returns(uint) {
-        block.timestamp - loan.takeoutTime;
+    // 5% per year in converted to seconds
+    PRBMath.UD60x18 private ratePerSecond = uint(5).fromUint().div(uint(100).fromUint()).div(secondsInYear.fromUint());
+
+    function loanSeconds(Loan memory loan) private view returns (PRBMath.UD60x18 memory) {
+        return (block.timestamp - loan.takeoutTime).fromUint();
     }
 
-    function borrowerDebt(Loan memory loan) private {
-        loan.principal * (1 + ratePerSecond * loanSeconds(loan)) - loan.totalRepaid;
+    function borrowerDebt(Loan memory loan) private view returns (PRBMath.UD60x18 memory) {
+        return loan.principal.mul(uint(1).fromUint().add(ratePerSecond.mul(loanSeconds(loan)))).sub(loan.totalRepaid);
     }
 }
