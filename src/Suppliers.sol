6306f479df25a657c8f7cd66da278d640949d98e
amy <amy@froggery.dev>
2022-11-28 00:45:20 +0100

    update stuff

--- src/Suppliers.sol
+++ src/Suppliers.sol
@@ -2,16 +2,12 @@
 pragma solidity ^0.8.15;
 
 import "lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
-import "./Math.sol";
-type eResidencyId is uint256;
 
-contract Savings is Math {
+contract Suppliers is Math {
 
-    mapping(eResidencyId => address) public borrowers;
     mapping(address => PRBMath.UD60x18) public supplied;
 
     IERC20 DAI;
-    address prosperaGsp;
 
     using SafeERC20 for IERC20;
     using PRBMathUD60x18Typed for PRBMath.UD60x18;
@@ -19,7 +15,7 @@ contract Savings is Math {
     // called by supplier
     function supply(PRBMath.UD60x18 memory deposit) external {
 
-        // Pull deposit from supplier
+        // Pull deposit from supplier to protocol
         DAI.safeTransferFrom(msg.sender, address(this), deposit.toUint());
 
         // Increase caller supplied // should I use receipt tokens instead to reward early suppliers?
@@ -32,17 +28,13 @@ contract Savings is Math {
     // called by supplier
     function withdraw(PRBMath.UD60x18 memory withdrawal) external {
 
+        // Decrease caller supplied // should I use receipt tokens instead to reward early suppliers?
+        supplied[msg.sender] = supplied[msg.sender].sub(withdrawal);
+
         // Decrease totalSupplied
         totalSupplied = totalSupplied.sub(withdrawal);
-    }
 
-    // called by GSP
-    function mint(eResidencyId propertyOwner, bytes calldata propertyInfo) external onlyProsperaGsp {
-        
-    }
-
-    modifier onlyProsperaGsp {
-        require(msg.sender == prosperaGsp, "caller not prospera");
-        _;
+        // Pull deposit from protocol to supplier
+        DAI.safeTransfer(msg.sender, withdrawal.toUint());
     }
 }
