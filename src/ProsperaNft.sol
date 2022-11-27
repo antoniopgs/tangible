84198ab0c08b0e72c1e481bfbd2f732ff7040ae4
amy <amy@froggery.dev>
2022-11-27 22:21:45 +0100

    update stuff

---
+++ src/ProsperaNft.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.15;
+
+import "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+
+contract ProsperaNft is ERC721 {
+
+    string[] inspections;
+
+}
