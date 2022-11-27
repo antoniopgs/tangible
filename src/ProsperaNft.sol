d5791d0fa593226a694c31c9ab0d70e4c1dc2889
amy <amy@froggery.dev>
2022-11-27 22:26:43 +0100

    update stuff

--- src/ProsperaNft.sol
+++ src/ProsperaNft.sol
@@ -5,6 +5,19 @@ import "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 
 contract ProsperaNft is ERC721 {
 
+    // metadata
     string[] inspections;
+    // restrict transfers to eResidents only
 
+    function addInspection() external /* onlyInspector */ {
+
+    }
+
+    function updateMetadata() external /* onlyOwner */ {
+
+    }
+
+    // function lastUpdateInspected() external view returns (bool) {
+    //     return inspections[inspections.length - 1].inspectionTime > metadata.lastUpdateTime;
+    // }
 }
