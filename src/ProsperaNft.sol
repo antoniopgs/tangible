4836d725f5fb99b8a5372fd7d34b62ac109a0138
amy <amy@froggery.dev>
2022-11-27 23:58:04 +0100

    update stuff

--- src/ProsperaNft.sol
+++ src/ProsperaNft.sol
@@ -1,23 +1,89 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.15;
 
-import "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "lib/openzeppelin-contracts/contracts/utils/Counters.sol";
+import "lib/openzeppelin-contracts/contracts/access/AccessControl.sol";
 
-contract ProsperaNft is ERC721 {
+contract ProsperaNft is ERC721URIStorage, AccessControl {
 
-    // metadata
-    string[] inspections;
-    // restrict transfers to eResidents only
-
-    function addInspection() external /* onlyInspector */ {
-
+    // Structs
+    struct Inspection {
+        address inspector;
+        string inspectionURI;
+        uint inspectionTime;
     }
 
-    function updateMetadata() external /* onlyOwner */ {
+    // Vars
+    Counters.Counter private _tokenIds;
+    mapping(uint => Inspection[]) public tokenIdInspections;
+    mapping(uint => uint) public tokenIdLastURIUpdateTime;
+    mapping(address => bool) public isEResident;
+    mapping(uint => address) public eResidentAddress;
 
+    // Roles
+    bytes32 public constant PAC = keccak256("PAC");
+    bytes32 public constant GSP = keccak256("GSP");
+    bytes32 public constant INSPECTOR = keccak256("INSPECTOR");
+
+    // Libs
+    using Counters for Counters.Counter;
+
+    constructor() ERC721("Prospera Real Estate Token", "PROSPERA") {}
+
+    function addInspection(uint tokenId, string memory newInspectionURI) external onlyRole(INSPECTOR) {
+        tokenIdInspections[tokenId].push(
+            Inspection({
+                inspector: msg.sender,
+                inspectionURI: newInspectionURI,
+                inspectionTime: block.timestamp
+            })
+        );
     }
 
-    // function lastUpdateInspected() external view returns (bool) {
-    //     return inspections[inspections.length - 1].inspectionTime > metadata.lastUpdateTime;
-    // }
+    function updateTokenURI(uint tokenId, string memory newTokenURI) external {
+        require(msg.sender == ownerOf(tokenId), "only token owner can update its metadata");
+        _setTokenURI(tokenId, newTokenURI);
+        tokenIdLastURIUpdateTime[tokenId] = block.timestamp;
+    }
+
+    function mint(address to, string memory _tokenURI) external onlyRole(GSP) returns (uint newTokenId) {
+        newTokenId = _tokenIds.current();
+        _safeMint(to, newTokenId);
+        _setTokenURI(newTokenId, _tokenURI);
+        _tokenIds.increment();
+    }
+
+    function lastUpdateInspected(uint tokenId) external view returns (bool) {
+        Inspection[] memory _tokenIdInspections = tokenIdInspections[tokenId];
+        return _tokenIdInspections[_tokenIdInspections.length - 1].inspectionTime > tokenIdLastURIUpdateTime[tokenId];
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, AccessControl) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256, /* firstTokenId */
+        uint256 batchSize
+    ) internal override {
+        require(isEResident[to], "receiver not eResident");
+        if (batchSize > 1) {
+            if (from != address(0)) {
+                _balances[from] -= batchSize;
+            }
+            if (to != address(0)) {
+                _balances[to] += batchSize;
+            }
+        }
+    }
+
+    function verifyEResident(uint eResidentId, address eResidentAddr) external onlyRole() {
+        require(!isEResident[eResidentAddr], "address already associated to an eResident");
+        require(eResidentAddr[eResidentId], "eResidentId already associated to an address");
+        isEResident[eResidentAddr] = true;
+        eResidentAddr[eResidentId] = eResidentAddr;
+    }
 }
