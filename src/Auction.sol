0dea80f6cca5ba5fd3636d108406a7d63d23fbb3
amy <amy@froggery.dev>
2022-12-08 18:02:24 -0500

    update stuff

--- src/Auction.sol
+++ src/Auction.sol
@@ -4,8 +4,9 @@ pragma solidity ^0.8.15;
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "./Math.sol";
 
-contract Auctions is ERC721Holder {
+contract Auctions is Math, ERC721Holder {
 
     struct Bidder {
         address addr;
@@ -19,7 +20,6 @@ contract Auctions is ERC721Holder {
     }
 
     IERC721 prosperaNftContract;
-    IERC20 DAI;
     mapping(uint => Auction) public auctions;
 
     using SafeERC20 for IERC20;
@@ -35,20 +35,34 @@ contract Auctions is ERC721Holder {
         auction.buyoutPrice = buyoutPrice;
     }
 
-    // called by borrower
-    // should we block seller from calling this?
-    function buyout(Auction calldata auction) external {
+    function buyout(uint tokenId) external { // called by borrower // should we block seller from calling this?
         
-        // Pull buyoutPrice from caller/borrower to seller
-        DAI.safeTransferFrom(msg.sender, auction.seller, auction.buyoutPrice);
+        // Get auction
+        Auction memory auction = auctions[tokenId];
+        
+        // Pull buyoutPrice from msg.sender/buyer to seller
+        USDC.safeTransferFrom(msg.sender, auction.seller, auction.buyoutPrice);
+
+        // Send NFT to msg.sender/buyer
+        prosperaNftContract.safeTransferFrom(address(this), msg.sender, tokenId);
+    }
+
+    function loanBuyout(uint tokenId) external { // called by borrower // should we block seller from calling this?
+        
+        // Get auction
+        Auction memory auction = auctions[tokenId];
+        
+        // Pull down payment from msg.sender/buyer to seller
+
+        // Send principal from protocol to seller
+
+        // Collateralize NFT
 
         // Start Loan
         // startLoan();
     }
 
-    // called by borrower
-    // should we block seller from calling this?
-    function bid(uint tokenId, uint newBid) external {
+    function bid(uint tokenId, uint newBid) external { // called by borrower // should we block seller from calling this?
 
         // Get highestBidder
         Bidder storage highestBidder = auctions[tokenId].highestBidder;
@@ -57,22 +71,69 @@ contract Auctions is ERC721Holder {
         require(newBid > highestBidder.bid, "new bid must be greater than current highest bid");
 
         // Refund highestBidder
-        DAI.safeTransfer(highestBidder.addr, highestBidder.bid);
+        USDC.safeTransfer(highestBidder.addr, highestBidder.bid);
 
-        // Pull bid from caller/borrower to protocol
-        DAI.safeTransferFrom(msg.sender, address(this), newBid);
+        // Pull bid from caller/bidder to protocol
+        USDC.safeTransferFrom(msg.sender, address(this), newBid);
 
         // Update highestBidder
         highestBidder.addr = msg.sender;
         highestBidder.bid = newBid;
     }
 
-    // called by seller
-    function acceptBid(Auction calldata auction) external {
+    function loanBid(uint tokenId, uint newBid) external { // called by borrower // should we block seller from calling this?
+
+        // Get highestBidder
+        Bidder storage highestBidder = auctions[tokenId].highestBidder;
+
+        // Ensure newBid > highestBidder
+        require(newBid > highestBidder.bid, "new bid must be greater than current highest bid");
+
+        // Refund highestBidder
+        USDC.safeTransfer(highestBidder.addr, highestBidder.bid);
+
+        // Pull bid from caller/borrower to protocol
+        USDC.safeTransferFrom(msg.sender, address(this), newBid);
+
+        // Update highestBidder
+        highestBidder.addr = msg.sender;
+        highestBidder.bid = newBid;
+    }
+
+    function acceptBid(Auction calldata auction) external { // called by seller
         require(msg.sender == auction.seller, "only seller can accept bids");
 
+        if (/* loanBid */) {
+            _acceptLoanBid();
+
+        } else {
+            _acceptBid();
+        }
+
         // Pull bid from protocol to seller
-        DAI.safeTransferFrom(address(this), auction.seller, auction.highestBidder.bid);
+        USDC.safeTransferFrom(address(this), auction.seller, auction.highestBidder.bid);
+
+    }
+
+    function _acceptBid(uint tokenId) private {
+
+        // Get auction
+        Auction memory auction = auctions[tokenId];
+
+        // Pull bid from protocol to seller
+        USDC.safeTransferFrom(address(this), auction.seller, auction.highestBidder.bid);
+
+        // Send NFT from protocol to bidder
+        prosperaNftContract.safeTransferFrom(address(this), auction.higherBidder.addr, tokenId);
+    }
+
+    function _acceptLoanBid() private {
+
+        // Send highest bid from protocol to seller
+
+        // Send principal from protocol to seller
+
+        // Collateralize NFT
 
         // Start Loan
         // startLoan();
@@ -81,10 +142,10 @@ contract Auctions is ERC721Holder {
     function startLoan(address seller, uint salePrice, uint principal, address borrower) private {
 
         // Pull principal from protocol to seller
-        DAI.safeTransferFrom(address(this), seller, principal);
+        USDC.safeTransferFrom(address(this), seller, principal);
 
         // Pull downPayment from borrower to seller
         uint downPayment = salePrice - principal;
-        DAI.safeTransferFrom(borrower, seller, downPayment);
+        USDC.safeTransferFrom(borrower, seller, downPayment);
     }
 }
