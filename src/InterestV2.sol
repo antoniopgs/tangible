437f2ed0dfd8f0c7d17b2551bb22d9412e766333
amy <amy@froggery.dev>
2023-03-16 13:10:49 +0200

    implement InterestV2.sol

---
+++ src/InterestV2.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.15;
+
+import "@prb/math/UD60x18.sol";
+
+contract InterestV2 {
+
+    // Math Vars // Todo: figure out how to line-up concavity with optimalUtilization
+    UD60x18 k1;
+    UD60x18 k2;
+
+    function utilization() public view returns(UD60x18) { // Note: will be inherited (only here for now so it compiles)
+
+    }
+
+    function borrowerRatePerSecond() private view returns(UD60x18) {
+        return borrowerApr().div(toUD60x18(365 days));
+    }
+
+    function borrowerApr() public view returns (UD60x18) {
+        return k1.add(k2.div(toUD60x18(1).sub(utilization())));
+    }
+}
