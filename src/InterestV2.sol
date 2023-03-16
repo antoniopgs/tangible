6f24e6e6a30503a2d5e596cad46206d87d913538
amy <amy@froggery.dev>
2023-03-16 13:18:23 +0200

    update stuff

--- src/InterestV2.sol
+++ src/InterestV2.sol
@@ -6,12 +6,12 @@ import "@prb/math/UD60x18.sol";
 contract InterestV2 {
 
     // Math Vars // Todo: figure out how to line-up concavity with optimalUtilization
+    UD60x18 optimalUtilization;
     UD60x18 k1;
     UD60x18 k2;
 
-    function utilization() public view returns(UD60x18) { // Note: will be inherited (only here for now so it compiles)
-
-    }
+    function utilization() public view returns(UD60x18) {} // Note: will later be inherited (only here for now so it compiles)
+    function lenderApy() public view returns(UD60x18) {} // Note: will later be inherited (only here for now so it compiles)
 
     function borrowerRatePerSecond() private view returns(UD60x18) {
         return borrowerApr().div(toUD60x18(365 days));
@@ -19,5 +19,17 @@ contract InterestV2 {
 
     function borrowerApr() public view returns (UD60x18) {
         return k1.add(k2.div(toUD60x18(1).sub(utilization())));
+    }
+
+    function tUsdcToUsdc() public view returns (UD60x18) {
+        
+        // If utilization <= optimal
+        if (utilization.lte(optimalUtilization)) {
+            return toUD60x18(1).add(lenderApy());
+
+        // If utilization > optimal (penalty slope)
+        } else {
+            return .div(optimalUtilization.sub(1));
+        }
     }
 }
