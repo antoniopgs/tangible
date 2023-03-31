ea5581354dd223203c8ad5b1498d43fbcd45d781
amy <amy@froggery.dev>
2023-03-31 10:05:51 +0300

    update stuff

--- src/BorrowingV3.sol
+++ src/BorrowingV3.sol
@@ -6,14 +6,12 @@ import { SD59x18, toSD59x18 } from "@prb/math/SD59x18.sol";
 
 contract BorrowingV3 {
 
-    // Time vars
+    // Time constants
     uint private constant yearSeconds = 365 days;
     uint private constant yearMonths = 12;
     uint private constant monthSeconds = yearSeconds / yearMonths; // Note: yearSeconds % yearMonths = 0 (no precision loss)
-
-    // Other vars
-    UD60x18 one = toUD60x18(1);
-
+    
+    // Structs
     struct Loan {
         UD60x18 ratePerSecond;
         uint maxDurationSeconds;
@@ -23,9 +21,19 @@ contract BorrowingV3 {
         uint lastPaymentTime;
     }
 
+    // Pool vars
+    uint totalPrincipal;
+    uint totalDeposits;
+    uint totalInterestOwed;
+
+    // Loan storage
     mapping(uint => Loan) public loans;
 
-    function startLoan(uint tokenId, uint principal, uint borrowerAprPct, uint maxDurationYears) public {
+    // Other vars
+    UD60x18 one = toUD60x18(1);
+
+    // Functions
+    function startLoan(uint tokenId, uint principal, uint borrowerAprPct, uint maxDurationYears) external {
 
         UD60x18 ratePerSecond = toUD60x18(borrowerAprPct).div(toUD60x18(100)).div(toUD60x18(yearSeconds));
 
@@ -39,8 +47,36 @@ contract BorrowingV3 {
             balance: principal,
             lastPaymentTime: block.timestamp // Note: no payment here, but needed so lastPaymentElapsedSeconds only counts from now
         });
+
+        // Update pool
+        totalPrincipal += principal;
+        totalInterestOwed += ;
+    }
+
+    function payLoan(uint tokenId, uint payment) external {
+
+        // Get Loan
+        Loan storage loan = loans[tokenId];
+
+        // Calculate accruedRate
+        UD60x18 accruedRate = loan.ratePerSecond.mul(toUD60x18(lastPaymentElapsedSeconds(loan)));
+
+        // Calculate interest
+        uint interest = fromUD60x18(toUD60x18(loan.balance).mul(accruedRate));
+
+        // Calculate repayment
+        uint repayment = payment - interest;
+
+        // Update loan
+        loan.balance -= repayment;
+
+        // Update pool
+        totalPrincipal -= repayment;
+        totalDeposits += interest;
+        totalInterestOwed -= interest;
     }
     
+    // Views
     function calculatePaymentPerSecond(uint principal, UD60x18 ratePerSecond, uint maxDurationSeconds) private view returns(UD60x18 paymentPerSecond) {
 
         // Calculate x
@@ -85,23 +121,5 @@ contract BorrowingV3 {
 
     function lastPaymentElapsedSeconds(Loan memory loan) private view returns(uint) {
         return block.timestamp - loan.lastPaymentTime;
-    }
-
-
-    function payLoan(uint tokenId, uint payment) private {
-
-        // Get Loan
-        Loan storage loan = loans[tokenId];
-
-        // Calculate interest
-        UD60x18 accruedRate = loan.ratePerSecond.mul(toUD60x18(lastPaymentElapsedSeconds(loan)));
-
-        uint interest = fromUD60x18(toUD60x18(loan.balance).mul(accruedRate));
-
-        // Calculate repayment
-        uint repayment = payment - interest;
-
-        // Update loan
-        loan.balance -= repayment;
     }
 }
