26165c85b56c11df5089681412228fb743410a56
amy <amy@froggery.dev>
2023-04-01 20:56:14 +0300

    update stuff

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,7 +3,21 @@ pragma solidity ^0.8.15;
 
 import "forge-std/Script.sol";
 import { console } from "forge-std/console.sol";
+import "../src/BorrowingV3.sol";
+import "../src/tUsdc.sol";
 
 contract DeployScript is Script {
+    
+    // Deploy protocol
+    BorrowingV3 borrowing = new BorrowingV3();
 
+    // Build tUsdcDefaultOperators;
+    address[] tUsdcDefaultOperators;
+    tUsdcDefaultOperators[0] = address(borrowing);
+
+    // Deploy tUSDC
+    tUsdc tUSDC = new tUsdc();
+
+    // Initialize protocol
+    borrowing.initialize(tUSDC);
 }
