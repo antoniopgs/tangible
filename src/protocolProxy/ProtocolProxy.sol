3d39af99a39578e674bd759e2398083d7e2211cf
amy <amy@froggery.dev>
2023-04-15 23:05:39 +0100

    update stuff

--- src/protocolProxy/ProtocolProxy.sol
+++ src/protocolProxy/ProtocolProxy.sol
@@ -4,9 +4,15 @@ pragma solidity ^0.8.15;
 import "../state/state/State.sol";
 import "@openzeppelin/contracts/proxy/Proxy.sol";
 
+import "forge-std/console.sol";
+
 contract ProtocolProxy is State, Proxy {
     
-    function _implementation() internal view override returns (address) {
-        return logicTargets[msg.sig];
+    function _implementation() internal view override returns (address target) {
+        console.log("_implementation...");
+        console.logBytes4(msg.sig);
+        console.log("logicTargets[msg.sig]:", logicTargets[msg.sig]);
+        target = logicTargets[msg.sig];
+        require(target != address(0), "sig has no target");
     }
 }
--- src/state/targetManager/TargetManager.sol
+++ src/state/targetManager/TargetManager.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.15;
 import "./ITargetManager.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+import "forge-std/console.sol";
+
 abstract contract TargetManager is ITargetManager, Ownable {
 
     mapping (bytes4 => address) public logicTargets;
@@ -13,9 +15,16 @@ abstract contract TargetManager is ITargetManager, Ownable {
     }
 
     function setSelectorsTarget(bytes4[] calldata selectorsArr, address target) external onlyOwner {
+        console.log("setSelectorsTarget...");
 
         for (uint256 i = 0; i < selectorsArr.length; i++) {
+            console.log("i:", i);
+            console.logBytes4(selectorsArr[i]);
+            console.log("target:", target);
+            console.log("logicTargets[selectorsArr[i]]:", logicTargets[selectorsArr[i]]);
             logicTargets[selectorsArr[i]] = target;
+            console.log("logicTargets[selectorsArr[i]]:", logicTargets[selectorsArr[i]]);
+            console.log("");
         }
     }
 
