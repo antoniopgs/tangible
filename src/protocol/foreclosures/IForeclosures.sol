e273d46697b69e5d6ae8ee2b5f5f9e5ec8fd41ed
amy <amy@froggery.dev>
2023-04-26 18:34:58 +0100

    update stuff

--- src/protocol/foreclosures/IForeclosures.sol
+++ src/protocol/foreclosures/IForeclosures.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.15;
 import "../state/state/IState.sol";
 
 interface IForeclosures is IState {
-    function adminForeclose(TokenId tokenId, UD60x18 salePrice) external;
+    function adminForeclose(TokenId tokenId, uint salePrice) external;
     // function foreclose(TokenId tokenId) external;
     // function chainlinkForeclose(TokenId tokenId) external;
 }
