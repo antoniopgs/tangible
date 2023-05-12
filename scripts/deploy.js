7bf6770fd9630e6b781577562192c38d5b4540bb
amy <amy@froggery.dev>
2023-05-12 07:23:06 +0100

    update stuff

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,32 +1,77 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const getMnemonicTeam = async () => {
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  // Get tangibleTeam
+  const tangibleTeamAddress = ethers.Wallet.fromMnemonic(process.env.MNEMONIC).address;
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  // Return fungifyTeam signer
+  return (await ethers.getSigner(tangibleTeamAddress));
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+module.exports = deploy = async () => {
+
+  // Get team
+  // const team = await getMnemonicFungifyTeam();
+  const team = await ethers.getSigner();
+
+  // ProtocolProxy
+  const ProtocolProxy = await ethers.getContractFactory("ProtocolProxy");
+  const protocolProxy = await ProtocolProxy.connect(team).deploy();
+
+  // tUsdc
+  const TUsdc = await ethers.getContractFactory("tUsdc");
+  const tUsdcDefaultOperators = [ protocolProxy.address ];
+  const tUsdc = await TUsdc.connect(team).deploy(tUsdcDefaultOperators);
+
+  // TangibleNft
+  const TangibleNft = await ethers.getContractFactory("TangibleNft");
+  const tangibleNft = await TangibleNft.connect(team).deploy(protocolProxy.address);
+
+  // MockUsdc
+  const MockUsdc = await ethers.getContractFactory("MockUsdc");
+  const mockUsdc = await MockUsdc.connect(team).deploy();
+
+  // ---------- IMPLEMENTATIONS ----------
+
+  // Auctions
+  const Auctions = await ethers.getContractFactory("Auctions");
+  const auctions = await Auctions.connect(team).deploy();
+
+  // Automation
+  const Automation = await ethers.getContractFactory("Automation");
+  const automation = await Automation.connect(team).deploy();
+
+  // Borrowing
+  // const Borrowing = await ethers.getContractFactory("Borrowing");
+  // const borrowing = await Borrowing.connect(team).deploy();
+
+  // Interest
+  const Interest = await ethers.getContractFactory("Interest");
+  const interest = await Interest.connect(team).deploy();
+
+  // Lending
+  const Lending = await ethers.getContractFactory("Lending");
+  const lending = await Lending.connect(team).deploy();
+
+
+  // ---------- SELECTORS ------------
+
+  // Auctions
+  const auctionSelectors = [];
+  protocolProxy.connect(team).setSelectorsTarget(auctionSelectors, auctions.address);
+
+  // Automation
+  const automationSelectors = [];
+  protocolProxy.connect(team).setSelectorsTarget(automationSelectors, automation.address);
+
+  // Borrowing
+  // const borrowingSelectors = [];
+  // protocolProxy.connect(team).setSelectorsTarget(borrowingSelectors, borrowing.address);
+
+  // Interest
+  const interestSelectors = [];
+  protocolProxy.connect(team).setSelectorsTarget(interestSelectors, interest.address);
+  
+  // Lending
+  const lendingSelectors = [];
+  protocolProxy.connect(team).setSelectorsTarget(lendingSelectors, lending.address);
+}
