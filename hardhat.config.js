bace26d97c7fede971de69092c6b7dd989692787
amy <amy@froggery.dev>
2023-05-12 07:29:15 +0100

    update stuff

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,11 @@
-require("@nomicfoundation/hardhat-toolbox");
-
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://mainnet.infura.io/v3/f36750d69d314e3695b7fe230bb781af"
+      }
+    }
+  }
 };
