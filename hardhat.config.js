fe1ed9028b681f4e76b0be94fa613ec352c35f41
amy <amy@froggery.dev>
2023-05-12 21:53:08 +0100

    update stuff

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,6 +9,16 @@ module.exports = {
       forking: {
         url: "https://mainnet.infura.io/v3/f36750d69d314e3695b7fe230bb781af"
       }
+    },
+    polygonMumbai: {
+      url: "https://polygon-mumbai.g.alchemy.com/v2/aK8s83vhhzG8PbGyJqLi160PgWD_9IbX",
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+        path: "m/44'/60'/0'/0",
+        initialIndex: 0,
+        count: 20,
+        passphrase: "",
+      }
     }
   },
   contractSizer: {
