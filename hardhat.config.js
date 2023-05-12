ea7e710b1bee5bc0c304dc1b2329cc2d0040dca9
amy <amy@froggery.dev>
2023-05-12 08:44:28 +0100

    add contract sizer

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,5 @@
+require('hardhat-contract-sizer');
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
@@ -7,5 +9,8 @@ module.exports = {
         url: "https://mainnet.infura.io/v3/f36750d69d314e3695b7fe230bb781af"
       }
     }
+  },
+  contractSizer: {
+    runOnCompile: true
   }
 };
