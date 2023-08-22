10ba93aa13667aa4831ae48d22668c98d96ce2ba
amy <amy@froggery.dev>
2023-08-22 21:55:28 +0100

    add sepolia network

--- hardhat.config.js
+++ hardhat.config.js
@@ -20,6 +20,16 @@ module.exports = {
         count: 20,
         passphrase: "",
       }
+    },
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/y7BrjLoYar43xDMfT298ttTLMcW4jTmu",
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+        path: "m/44'/60'/0'/0",
+        initialIndex: 0,
+        count: 20,
+        passphrase: "",
+      }
     }
   },
   contractSizer: {
