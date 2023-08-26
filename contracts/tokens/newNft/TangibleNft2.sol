8652336d41a061cefd822631d99c6a2ca28127d8
amy <amy@froggery.dev>
2023-08-26 16:18:37 +0100

    block transfer of nfts with mortgage debt

--- contracts/tokens/newNft/TangibleNft2.sol
+++ contracts/tokens/newNft/TangibleNft2.sol
@@ -40,8 +40,9 @@ contract TangibleNft2 is ITangibleNft2, ERC721URIStorage, ERC721Enumerable, Resi
     // Todo:
     // - require payment of transfer fees & sale fees before transfer? Or just build up debt for later?
     // - how to transfer tokens with debt?
-    function _beforeTokenTransfer(address from, address to, uint256, /* firstTokenId */ uint256 batchSize) internal override(ERC721, ERC721Enumerable) {
+    function _beforeTokenTransfer(address from, address to, uint256 firstTokenId, uint256 batchSize) internal override(ERC721, ERC721Enumerable) {
         require(_isResident(to), "receiver not resident");
+        require(tokenDebts[firstTokenId].loan.unpaidPrincipal == 0, "can't transfer nft with mortgage debt");
         super._beforeTokenTransfer(from, to, 0, batchSize); // is it fine to pass 0 here?
     }
 
