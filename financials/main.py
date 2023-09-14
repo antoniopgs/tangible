c2243bfdb2270a3fb653398d3594f039984ea43d
António Gonçalves <apgoncalves@outlook.pt>
2023-09-14 13:15:17 +0100

    update stuff

--- financials/main.py
+++ financials/main.py
@@ -1,26 +1,17 @@
+# Imports
 import json
 from data import yearlyNewLoans
 
-interestFee = 0.02
-
+# Classes
 class Loan:
 
     yearSeconds = 365 * 24 * 60 * 60
 
-    def __init__(self, principal, apr, maxYears):
+    def __init__(self, unitPrice, ltv, apr, maxYears):
 
         # Calculate Vars
-        ratePerSecond, maxSeconds, paymentPerSecond = self.calculateVars(principal, apr, maxYears)
-
-        # Increment Loan Counter
-        Loan.loanCounter = Loan.loanCounter + 1
-
-        # Store Instance Vars
-        self.id = Loan.loanCounter
-        self.principal = principal
-        self.ratePerSecond = ratePerSecond
-        self.maxSeconds = maxSeconds
-        self.paymentPerSecond = paymentPerSecond
+        self.principal = unitPrice * ltv
+        self.ratePerSecond, self.maxSeconds, self.paymentPerSecond = self.calculateVars(self.principal, apr, maxYears)
 
     def calculateVars(self, principal, apr, maxYears):
 
@@ -43,9 +34,11 @@ class Loan:
 
 class LoanGroup(Loan):
     
-    def __init__(self, principal, apr, maxYears, units, mortgageNeed):
-        Loan.__init__(self, principal, apr, maxYears)
+    def __init__(self, unitPrice, ltv, apr, maxYears, units, mortgageNeed):
+        Loan.__init__(self, unitPrice, ltv, apr, maxYears)
         self.loanCount = units * mortgageNeed
+        principal = unitPrice * ltv
+        self.combinedPrincipal = self.loanCount * principal
 
     def combinedBalanceAt(self, second):
         return self.loanCount * self.balanceAt(second)
@@ -62,74 +55,46 @@ class LoanGroup(Loan):
         yearEnd = year * Loan.yearSeconds
         return self.combinedInterestPaidAt(yearEnd) - self.combinedInterestPaidAt(yearStart)
 
-    def grossCombinedApy(self, year):
-        combinedPrincipal = self.loanCount * self.principal
-        return self.combinedYearlyInterest(year) / combinedPrincipal
-
-    def netCombinedApy(self, year):
-        return (1 - interestFee) * self.grossCombinedApy(year)
-
-    def netCombinedApyPct(self, year):
-        return self.netCombinedApy(year) * 100
-
-    
-
-    
+    def combinedPrincipalAt(self, year):
+        yearEndSeconds = year * Loan.yearSeconds
+        return self.combinedPrincipal if yearEndSeconds <= self.maxSeconds else 0
 
 
-# Yearly New Loans
+# Standalone Functions
+def apy(interest, principal):
+    return interest / principal if principal > 0 else 0
+
+def netApy(interest, principal):
+    return (1 - interestFee) * apy(interest, principal)
+
+def netApyPct(interest, principal):
+    return netApy(interest, principal) * 100
+
+
+# Main Logic
+interestFee = 0.02
 years = 10
-activeLoans = []
+loanGroups = []
+for year in range(1, years + 1):
 
-for y in range(1, years):
-
-    print(f"----- YEAR: {y} -----")
+    print(f"----- YEAR: {year} -----")
 
     # If there are newLoans
-    newLoans = yearlyNewLoans.get(y)
+    newLoans = yearlyNewLoans.get(year)
     if (newLoans is not None):
 
-        # Calculate newLoans vars
-        newLoansAmount = newLoans["mortgageNeed"] * newLoans["units"]
-        combinedPrincipal = newLoansAmount * newLoans["avgPrice"] * newLoans[
-            "ltv"]
-
-        print(f"- newLoans: {newLoansAmount}")
-        print(f"- combinedPrincipal: {combinedPrincipal}")
-
-        # Push newLoans to activeLoans
-        activeLoans.append(
-            Loan(combinedPrincipal, newLoans["apr"], newLoans["maxYears"]))
-
-        totalPrincipal += combinedPrincipal
-
-    print()
-
-    # Loop activeLoans
-    combinedInterestPayments = 0
-    for loan in activeLoans:
-        print(f"Loan {loan.id}:")
-        print(
-            f"- balance at start of year {loan.currentYear}: {loan.balanceAtSecond(loan.yearStartTime(loan.currentYear))}"
+        # Add new LoanGroup
+        loanGroups.append(
+            LoanGroup(newLoans["avgPrice"], newLoans["ltv"], newLoans["apr"], newLoans["maxYears"], newLoans["units"], newLoans["mortgageNeed"])
         )
-        combinedInterestPayments += loan.calculateYearlyInterestPaid()
-        loan.incrementYear()
 
-    print()
+    # Loop Loan Groups
+    allLoanGroupsYearlyInterest = 0
+    allLoanGroupsYearlyPrincipal = 0
+    for loanGroup in loanGroups:
+        allLoanGroupsYearlyInterest += loanGroup.combinedYearlyInterest(year)
+        allLoanGroupsYearlyPrincipal += loanGroup.combinedPrincipalAt(year)
 
-    tangibleInterestProfit = interestFee * combinedInterestPayments
-    lenderProfit = combinedInterestPayments - tangibleInterestProfit
-    lenderApy = lenderProfit * 100 / totalPrincipal
-
-    print(f"lenderProfit: {lenderProfit}")
-    print(f"totalPrincipal: {totalPrincipal}")
-
-    yearlyInterestPayments[y] = {
-        "tangibleInterestProfit": interestFee * combinedInterestPayments,
-        "lenderProfit": lenderProfit,
-        "lenderApy": lenderApy
-    }
-
-print(json.dumps(yearlyInterestPayments, indent=4))
-
-# Todo: remove loans from activeLoans
+    # Print
+    print(f"- Net Apy: {netApyPct(allLoanGroupsYearlyInterest, allLoanGroupsYearlyPrincipal)}%\n")
+    
