4f8ee80ffd64572832397ac8e55e8eea3849810a
António Gonçalves <apgoncalves@outlook.pt>
2023-09-24 17:05:53 +0100

    update Math.sol

--- contracts/Math.sol
+++ contracts/Math.sol
@@ -5,18 +5,32 @@ import { Loan } from "./types/Types.sol";
 
 contract Math {
 
+    // Time constants
+    uint public constant yearSeconds = 365 days;
+    uint public constant yearMonths = 12;
+    uint public constant monthSeconds = yearSeconds / yearMonths; // Note: yearSeconds % yearMonths = 0 (no precision loss)
+
     function balanceAt(Loan memory loan, uint second) private view returns(uint) {
-        return loan.paymentPerSecond * (1 - (1 + loan.ratePerSecond) ** (second - loan.maxDurationSeconds)) / loan.ratePerSecond;
+        return (loan.paymentPerSecond * (1 - (1 + loan.ratePerSecond) ** (second - loan.maxDurationSeconds))) / loan.ratePerSecond;
     }
 
     function principalCapAt(Loan memory loan, uint loanMonth) public view returns(uint) {
-        lastMonth = loan.maxDurationSeconds / monthSeconds;
-        require(loanMonth >= 1 && loanMonth <= lastMonth, "invalid loanMonth");
+        require(loanMonth >= 1 && loanMonth <= loan.maxMonths, "invalid loanMonth");
         uint loanMonthStart = (loanMonth - 1) * monthSeconds;
         return balanceAt(loan, loanMonthStart);
     }
 
-    function loanMonth(Loan memory loan, uint currentMonth) public view returns(uint) {
+    function loanCurrentMonth(Loan memory loan) public view returns(uint) {
+        uint activeTime = block.timestamp - loan.startTime;
+        return (activeTime / monthSeconds) + 1;
+    }
 
+    function currentPrincipalCap(Loan memory loan) private view returns(uint) {
+        return principalCapAt(loan, loanCurrentMonth(loan));
+    }
+
+    function defaulted(Loan memory loan) private view returns(bool _defaulted, uint defaultTime) {
+
+        _defaulted = loan.unpaidPrincipal > currentPrincipalCap(loan);
     }
 }
