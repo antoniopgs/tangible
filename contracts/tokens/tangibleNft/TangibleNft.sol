e61aa803d557398399facb9a69b520e93ac3da13
António Gonçalves <apgoncalves@outlook.pt>
2023-10-27 14:43:51 +0100

    update stuff

--- contracts/tokens/tangibleNft/TangibleNft.sol
+++ contracts/tokens/tangibleNft/TangibleNft.sol
@@ -4,15 +4,12 @@ pragma solidity ^0.8.15;
 import "./ITangibleNft.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-// import "./residents/Residents.sol";
 import "../../protocol/info/IInfo.sol";
 import "@openzeppelin/contracts/access/IAccessControl.sol";
 import { PAC } from "../../types/RoleNames.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-// - receiver must be resident
-// - cannot transfer token with debt
-// Todo: require payment of transfer fees & sale fees before transfer? Or just build up debt for later?
-// Note: override to allow protocol/PAC to move tokens (don't use operators, as they can be revoked)
+// Todo: override to allow protocol/PAC to move tokens (don't use operators, as they can be revoked)
 contract TangibleNft is ITangibleNft, ERC721URIStorage, ERC721Enumerable {
 
     address immutable protocolProxy;
@@ -35,20 +32,28 @@ contract TangibleNft is ITangibleNft, ERC721URIStorage, ERC721Enumerable {
         return tokenId < totalSupply();
     }
 
-    // Inheritance Overrides
-    function _increaseBalance(address account, uint128 value) internal override(ERC721, ERC721Enumerable) {
-
+    // Question: require payment of transfer fees & sale fees before transfer? Or just build up debt for later?
+    function _update(address to, uint256 tokenId, address auth) internal override(ERC721, ERC721Enumerable) returns (address) {
+        require(IInfo(protocolProxy).isResident(to), "receiver not resident");
+        require(IInfo(protocolProxy).unpaidPrincipal(tokenId) == 0, "cannot transfer token with debt");
+        return super._update(to, tokenId, auth);
     }
 
-    function _update(address to, uint256 tokenId, address auth) internal override(ERC721, ERC721Enumerable) returns (address) {
+    function isApprovedForAll(address owner, address operator) public view override(ERC721, IERC721) returns (bool) {
+        return super.isApprovedForAll(owner, operator) 
+        || IAccessControl(protocolProxy).hasRole(PAC, operator); // Note: PAC should always be able to transfer, regardless of approvals
+    }
 
+    // Inheritance Overrides
+    function _increaseBalance(address account, uint128 value) internal override(ERC721, ERC721Enumerable) {
+        super._increaseBalance(account, value);
     }
 
     function supportsInterface(bytes4 interfaceId) public view override(ERC721Enumerable, ERC721URIStorage) returns (bool) {
-
+        return super.supportsInterface(interfaceId);
     }
 
     function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
-
+        return super.tokenURI(tokenId);
     }
 }
